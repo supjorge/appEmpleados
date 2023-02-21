bcec3f58c1b3eb0cacede7d0d42c6b4bc2b48497
Jorge Ruiz <jor.rui.cer@gmail.com>
2023-02-20 18:34:54 -0600

    First commit, added all files from local repository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appEmpleados';
+  title = 'Listado de Empleados';
+  empleados:Empleado[]=[
+    new Empleado("Jorge","Ruiz","CEO",20000),
+    new Empleado("Ramon","Juarez","HR",10000),
+    new Empleado("Carlos","Ramirez","DEVELOPER",12000),
+    new Empleado("Marcos","Alonso","DRIVER",8000)
+  ]
+  cuadroNombre=""
+  cuadroApellido=""
+  cuadroCargo=""
+  cuadroSalario=0
+
+  agregarEmpleado(){
+    let miEmpleado=new Empleado(this.cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario)
+    this.empleados.push(miEmpleado)
+  }
 }
